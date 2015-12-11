10bdd4158fed180490fe789bdc9a9b1322ee0607
dario <darioyzf@gmail.com>
2015-12-11 23:34:27 +0100

    oki

--- test/main.js
+++ test/main.js
@@ -1,37 +1,41 @@
 
-var assert = require('chai').assert;
+var expect = require('chai').expect;
 var HAPD=require('../index');
 var verb=require('verbo');
 
 
-before('get status info',function() {
-  return HAPD({
-    path:'/tmp/hapd.conf',
-    interface:'wlan0',
-    ssid:'testssid',
-    test:true
-  }).then(function(data){
-verb(data,'info','Status Info')
-json=data
-  }).catch(function(err){
-    console.log(err)
-  })
-});
+
 
 describe('Status Object', function() {
+  this.timeout(50000);
+
+  before('get status info',function() {
+    return HAPD({
+      path:'/tmp/hapd.conf',
+      interface:'wlan0',
+      ssid:'testssid',
+      test:true
+    }).then(function(data){
+      json=data
+    }).catch(function(err){
+      console.log(err)
+    })
+  });
+
   describe('check json', function () {
 
-    it('validate ', function(){
-
-        assert.isObject(json, 'Status is an object');
-
-
+    it('exist', function(){
+      expect(json).to.be.ok;
     })
-    it('validate interface', function(){
-        assert.isString(json.interface);
+
+    it('is an object ', function(){
+      expect(json).is.an('object');
     })
-    it('validate driver', function(){
-      assert.isString(json.driver);
+    it('have an interface', function(){
+      expect(json).to.have.property('interface').that.is.a('string');
+    })
+    it('have a driver', function(){
+      expect(json).to.have.property('driver').that.is.a('string');
     })
   });
 });
